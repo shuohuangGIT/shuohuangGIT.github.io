0186006f7c62e3f502d737fbe57b3d0b36a56a5f
Shuo Huang <heysure567@gmail.com>
2024-06-10 22:34:06 +0200

    223306102024

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-// const image1 = document.getElementById('image1');
+const image1 = document.getElementById('image1');
 // const image2 = document.getElementById('image2');
 // const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
 // Dark or Light Images
-// function imageMode(color) {
-//     image1.src = `img/undraw_proud_coder_${color}.svg`;
-//     image2.src = `img/undraw_feeling_proud_${color}.svg`;
-//     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
-// };
+function imageMode(color) {
+    image1.src = `img/me_${color}.jpg`;
+    // image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    // image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+};
 
 function toggleDarkLightMode(isDark) {
     nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
